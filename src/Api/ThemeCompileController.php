d2263c2e0c473fae5236db3e514a5282070310a5
dneustadt <kontakt@davidneustadt.de>
2020-10-29 00:46:33 +0100

    inject sales channel repository in theme compile controller

--- src/Api/ThemeCompileController.php
+++ src/Api/ThemeCompileController.php
@@ -3,6 +3,7 @@
 namespace Dne\CustomCssJs\Api;
 
 use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\SalesChannel\SalesChannelCollection;
@@ -17,11 +18,20 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ThemeCompileController extends AbstractController
 {
+    /**
+     * @var ThemeService
+     */
     private $themeService;
 
-    public function __construct(ThemeService $themeService)
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $salesChannelRepository;
+
+    public function __construct(ThemeService $themeService, EntityRepositoryInterface $salesChannelRepository)
     {
         $this->themeService = $themeService;
+        $this->salesChannelRepository = $salesChannelRepository;
     }
 
     /**
@@ -51,7 +61,7 @@ class ThemeCompileController extends AbstractController
         $criteria->addAssociation('themes');
 
         /** @var SalesChannelCollection $result */
-        $result = $this->container->get('sales_channel.repository')->search($criteria, $context)->getEntities();
+        $result = $this->salesChannelRepository->search($criteria, $context)->getEntities();
 
         return $result;
     }
