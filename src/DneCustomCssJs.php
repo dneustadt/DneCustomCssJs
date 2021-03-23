cfc8bb9a6378d322ec62bba015f252555219f6dd
dneustadt <kontakt@davidneustadt.de>
2021-03-23 05:59:01 +0100

    use new theme compiler events to inject resources - resolves #9

--- src/DneCustomCssJs.php
+++ src/DneCustomCssJs.php
@@ -2,20 +2,11 @@
 
 namespace Dne\CustomCssJs;
 
-use Dne\CustomCssJs\Components\CompilerPass\ReplaceThemeCompilerPass;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\UninstallContext;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class DneCustomCssJs extends Plugin
 {
-    public function build(ContainerBuilder $container): void
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new ReplaceThemeCompilerPass());
-    }
-
     public function uninstall(UninstallContext $uninstallContext): void
     {
         if (!$uninstallContext->keepUserData()) {
